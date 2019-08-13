9960d30d9d99e8662f875179573fa981cefe0b2c
ashish1sasmal <ashishsasmal1@gmail.com>
2019-08-13 17:28:01 +0530

    models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+	list_display=['title','author','date_posted']
+
+admin.site.register(Post,PostAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,16 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,ListView
 
-class HomeView(TemplateView):
+class HomeView(ListView):
 	template_name='blog/home.html'
-	posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
-
-	def get_context_data(self, **kwargs):
-		d = {'posts':self.posts}
-		return d
+	context_object_name='posts'
+	model=Post
+	# def get_context_data(self, **kwargs):
+	# 	d = {'posts':self.posts}
+	# 	return d
 
 class AboutView(TemplateView):
 	template_name='blog/about.html'
