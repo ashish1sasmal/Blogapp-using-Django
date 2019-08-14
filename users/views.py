c57175e66b183d1f55c8016fdaea53a392766e6b
ashish1sasmal <ashishsasmal1@gmail.com>
2019-08-15 01:41:52 +0530

    user_profile_pic+custom_user_creation form

--- users/views.py
+++ users/views.py
@@ -2,20 +2,28 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm,UserForm
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
+        form = UserForm(data=request.POST)
+        profile_form=UserRegisterForm(data=request.POST)
+        if form.is_valid() and profile_form.is_valid():
+            user=form.save()
+            user.save()
+            profile=profile_form.save(commit=False)
+            profile.user=user
+            if 'image' in request.FILES:
+                profile.image=request.FILES['image']
+            profile.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Your account has been created! You are now able to log in')
             return redirect('login')
     else:
-        form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+        form = UserForm()
+        profile_form=UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form,'profile_form':profile_form})
 
 
 @login_required
