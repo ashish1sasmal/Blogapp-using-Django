beb33b70c4b7acb67f389ce0e6d7fe8a9c1eb078
ashish1sasmal <ashishsasmal1@gmail.com>
2019-08-15 02:19:07 +0530

    profile_update+profile_pic_resize

--- users/forms.py
+++ users/forms.py
@@ -17,3 +17,14 @@ class UserRegisterForm(forms.ModelForm):
 	class Meta():
 		model=Profile
 		fields=('image',)
+
+class UserUpdateForm(forms.ModelForm):
+	email = forms.EmailField()
+	class Meta:
+		model = User
+		fields = ['username', 'email',]
+
+class ProfileUpdateForm(forms.ModelForm):
+	class Meta:
+		model=Profile
+		fields=('image',)
