06894825ab7f461a98e29763409f421b19d63f09
ashish1sasmal <ashishsasmal1@gmail.com>
2019-08-14 11:39:55 +0530

    auth_views+login+logout+profile

--- django_project/settings.py
+++ django_project/settings.py
@@ -133,3 +133,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK='bootstrap4'
+
+LOGIN_REDIRECT_URL='blog-home'
+LOGIN_URL='login'
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from .forms import RegisterationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -12,8 +13,11 @@ def Register(request):
 			form.save()
 			username=form.cleaned_data.get('username')
 			messages.success(request,f'Account Created for {username}!')
-			return redirect('blog-home')
+			return redirect('login')
 	else:
 		form=RegisterationForm()
 
 	return render(request,'users/register.html',{'form':form})
+@login_required
+def profile(request):
+	return render(request,'users/profile.html')
