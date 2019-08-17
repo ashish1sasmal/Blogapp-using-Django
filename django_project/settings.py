28b103eaec12e370d9bc8edc69572ee6c42b4ae1
ashish1sasmal <ashishsasmal1@gmail.com>
2019-08-18 01:54:06 +0530

    fb login

--- django_project/settings.py
+++ django_project/settings.py
@@ -32,16 +32,28 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'blog',
     'users',
     'crispy_forms',
+
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.facebook',
+    
+    
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -71,6 +83,8 @@ TEMPLATES = [
     },
 ]
 
+
+
 WSGI_APPLICATION = 'django_project.wsgi.application'
 
 
@@ -84,7 +98,13 @@ DATABASES = {
     }
 }
 
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
 
+
+    'allauth.account.auth_backends.AuthenticationBackend',
+
+)
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -96,7 +116,7 @@ PASSWORD_HASHERS=[
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
 ]
 
-
+SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
 
 AUTH_PASSWORD_VALIDATORS = [
     {
