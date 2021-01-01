3efa33c7f84f34128ea33c6854058f642b2fa83b
ashish1sasmal <ashishsasmal1@gmail.com>
2021-01-01 17:49:25 +0530

    deplot-->heroku

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +27,7 @@ SECRET_KEY = 'jn@@43@u71g7!3-)i1mz*t9%)_il)gw60=!#awi0-6y-yqzf50'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -169,3 +170,4 @@ CRISPY_TEMPLATE_PACK='bootstrap4'
 LOGIN_REDIRECT_URL='blog-home'
 LOGIN_URL='login'
 LOGOUT_URL = 'logout'
+django_heroku.settings(locals())
